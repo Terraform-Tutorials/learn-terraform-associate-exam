61b63578949149696551dd741a87cd3d4625e578
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-12-21 14:29:52 -0300

    updates

--- terraform-labs/aws/miscellaneous/terraform-block.tf
+++ terraform-labs/aws/miscellaneous/terraform-block.tf
@@ -10,4 +10,27 @@ resource "aws_accessanalyzer_analyzer" "name" {
   
 }
 
+//tipo de data que precisamos
+//busca informacoes de fora do terraform para ser usado dentro do terraform
+data "aws_acm_certificate" "name" {
+  
+}
+
+module "name" {
+  
+}
+
+variable "amaurybsouza" {
+  
+}
+
+//pega informacoes de dentro do terraform para mostrar no momento de execucao
+output "name" {
+  
+}
+
+//pega funcoes e expressoes e nomeia as mesmas, assim voce pode chamar pelo nome, evitando repetir.
+locals {
+  
+}
 
