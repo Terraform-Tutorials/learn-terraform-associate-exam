bc15ebeac8ae17df12245c0aaff60be020c6b133
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-12-28 08:26:02 -0300

    matching some resources inside terraform

--- terraform-labs/azure/azure-storage-account/storage_account.tf
+++ terraform-labs/azure/azure-storage-account/storage_account.tf
@@ -1,14 +1,14 @@
 # Create a resource group
 resource "azurerm_resource_group" "my_resource_group" {
-  name     = ""
+  name     = "storage_account_resource_group"
   location = var.location
 
   tags = ""
 }
 
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = ""
-  resource_group_name      = ""
+  name                     = "amaurystorageaccount"
+  resource_group_name      = azurerm_resource_group.my_resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -20,5 +20,5 @@ resource "azurerm_storage_account" "my_storage_account" {
 
 resource "azurerm_storage_container" "my_storage_container" {
   name                  = ""
-  storage_account_name  = ""
+  storage_account_name  = azurerm_storage_account.my_storage_account.name
 }
