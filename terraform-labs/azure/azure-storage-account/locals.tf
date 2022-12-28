7e610206bed24f2f8756f4ab03b5144bc69bd2eb
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-12-28 08:46:19 -0300

    updates

---
+++ terraform-labs/azure/azure-storage-account/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    owner      = "Amaury"
+    managed_by = "terraform"
+  }
+}
--- terraform-labs/azure/azure-storage-account/storage_account.tf
+++ terraform-labs/azure/azure-storage-account/storage_account.tf
@@ -3,22 +3,20 @@ resource "azurerm_resource_group" "my_resource_group" {
   name     = "storage_account_resource_group"
   location = var.location
 
-  tags = ""
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "amaurystorageaccount"
+  name                     = "amaurybstorageaccount"
   resource_group_name      = azurerm_resource_group.my_resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = {
-    environment = ""
-  }
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_container" "my_storage_container" {
-  name                  = ""
-  storage_account_name  = azurerm_storage_account.my_storage_account.name
+  name                 = "amaurycontainer"
+  storage_account_name = azurerm_storage_account.my_storage_account.name
 }
